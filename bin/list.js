4e79dae55c1cb3fba66a3a2548a66bdf2dc5a241
inpm <inpm.cy@gmail.com>
2019-04-04 14:55:27 +0800

    list功能基本完成

--- bin/list.js
+++ bin/list.js
@@ -1,20 +1,38 @@
 #!/usr/bin/env node
 
-const co = require('co')
-const prompt = require('co-prompt')
 const chalk = require('chalk')
+const inquirer = require('inquirer')
+const download = require('download-git-repo')
+const ora = require('ora')
 const tempaltes = require('../lib/templates')
 
+const loading = ora({
+  text: 'loading',
+  color: 'yellow'
+})
+
 module.exports = () => {
-  for(let key in tempaltes.list) {
-    let temp = tempaltes.list[key]
-    console.log(
-      ' ' + chalk.yellow('⭐️') + 
-      ' ' + chalk.green(temp.name) +
-      '-' + temp.desc
-    )
-  }
-  if(!tempaltes.list || tempaltes.list.length===0) {
-    console.log(chalk.grey('当前无项目模版可用'))
+  if(!tempaltes || tempaltes.length===0) {
+    console.log(chalk.yellow('当前无项目模版可用'))
+  } else {
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'list',
+        message: '选择项目模版?',
+        choices: tempaltes
+      }
+    ]).then((res) => {
+      tempaltes.map(v => {
+        if(v.name === res.list) {
+          const url = v.url
+          loading.start()
+          download(`direct:${url}`, res.list, {clone: true}, (err) => {
+            loading.stop()
+            console.log(err ? chalk.red('Error') : chalk.green('Success'))
+          })
+        }
+      })
+    })
   }
 }
