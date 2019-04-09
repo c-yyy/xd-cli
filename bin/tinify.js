1293a4b2c66c21495787ffbc2ee2db583900ec38
inpm <inpm.cy@gmail.com>
2019-04-09 15:48:42 +0800

    新增tinify图片压缩

--- bin/tinify.js
+++ bin/tinify.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
 
 const rootPath = process.cwd()
-const images = require('images')
 const fs = require('fs')
 const path = require('path')
 const chalk = require('chalk')
+const tinify = require('tinify')
+tinify.key = 'uGMI6gif8PB0pP7XvLblVsMnBXQIvcBt'
 const ora = require('ora')
 const loading = ora({
   text: 'loading',
-  color: 'blue'
+  color: 'yellow'
 })
 
 rarImages(rootPath)
@@ -23,9 +24,17 @@ async function rarImages(rootPath) {
       if(isDir) {
         rarImages(fullPath)
       } else {
-        images(fullPath).save(fullPath, {quality:50})
-        loading.stop()
-        console.log(chalk.grey(fullPath)+chalk.green('🚀 Success!'))
+        fs.readFile(fullPath, (err, sourceData) => {
+          if(err) console.log(chalk.red(err))
+          tinify.fromBuffer(sourceData).toBuffer((err, resultData) => {
+            if(err) console.log(chalk.red(err))
+            fs.writeFile(fullPath, resultData, (err) => {
+              if(err) console.log(chalk.red(err))
+              loading.stop()
+              console.log(chalk.grey(fullPath)+chalk.green('🚀 Success!'))
+            })
+          })
+        })
       }
     } catch (err) {
       return console.log(chalk.red(err))
@@ -51,3 +60,24 @@ function readDir(path) {
     })
   })
 }
+
+function toBufferPromise(img) {
+  fs.readFile(img, (sourceData, err) => {
+    if(err) throw err
+    tinify.fromBuffer(sourceData).toBuffer((err, resultData) => {
+      if(err) throw err
+      return resultData
+    })
+  })
+}
+
+function writeFilePromise(file, content, cb) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, content, (err) => {
+      if(err) throw err
+      cb && cb()
+      resolve()
+    })
+  })
+}
+
