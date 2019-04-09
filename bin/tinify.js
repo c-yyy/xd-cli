1e7d846aec4bd086126c12b1e3f1965e3af6b312
inpm <inpm.cy@gmail.com>
2019-04-09 13:18:41 +0800

    图片压缩基本功能完成 对文件的递归调用还有问题 还有就是感觉速度有点慢

--- bin/tinify.js
+++ bin/tinify.js
@@ -8,23 +8,43 @@ const tinify = require('tinify')
 tinify.key = 'uGMI6gif8PB0pP7XvLblVsMnBXQIvcBt'
 const ora = require('ora')
 const loading = ora({
-  text: 'loading',
-  color: 'yellow'
+  text: '',
+  color: 'blue'
 })
 
-function tinifyImg(filePath) {
-  fs.stat(filePath, (err, stats) => {
-    if(err) throw err
-    const isDir = stats.isDirectory()
-    if(isDir) {
-      fs.readdir(filePath, (err, files) => {
-        if(err) throw err
-        console.log(1,files)
-      })
-    } else {
-      console.log(chalk.red('请在图片根目录下进行'), rootPath)
-    }
-  })
-}
-
 tinifyImg(rootPath)
+
+function tinifyImg(filePath) {
+  try {
+    fs.readdir(filePath, (err, files) => {
+      if(err) throw err
+      files.map(file => {
+        fs.stat(file, (err, stat) => {
+          if(err) throw err
+          loading.start()
+          const isFile = stat.isFile()
+          const isDir = stat.isDirectory()
+          const fileDir = path.join(filePath, file)
+          if(isFile) {
+            fs.readFile(file, (err, sourceData) => {
+              if(err) throw err
+              tinify.fromBuffer(sourceData).toBuffer((err, resultData) => {
+                if(err) throw err
+                fs.writeFile(file, resultData, () => {
+                  console.log(chalk.green(file+'__@success!'))
+                  loading.stop()
+                })
+              })
+            })
+          } else if(isDir) {
+            tinifyImg(fileDir)
+          } else {
+            console.log(chalk.red('文件格式不对'))
+          }
+        })
+      })
+    })
+  } catch(err) {
+    return
+  }
+}
